005174ab0ebb8fe77070cf856eeed2ad94f458be
Bart Jan van Assen <bartjanvassen@gmail.com>
2016-10-26 20:06:49 +0200

    fix window change aspect bug

--- src/Application.js
+++ src/Application.js
@@ -57,8 +57,8 @@ export default class Application {
         this.renderer.render( this.scene, this.camera );   
     }
     onWindowResize() {
-        camera.aspect = window.innerWidth / window.innerHeight;
-        camera.updateProjectionMatrix();
-        renderer.setSize( window.innerWidth, window.innerHeight );
+        this.camera.aspect = window.innerWidth / window.innerHeight;
+        this.camera.updateProjectionMatrix();
+        this.renderer.setSize( window.innerWidth, window.innerHeight );
     }
 }
